48f1921e9bfcf72a9b7df6c60ea3e660308e1108
HarpreetSingh123-dev <hsk54088@gmail.com>
2019-08-28 12:41:09 +0530

    rectangle.js

---
+++ rectangle.js
@@ -0,0 +1,10 @@
+//var rect = {
+    //perimeter: (x, y) => (2*(x+y)),
+    //area: (x, y) => (x*y)
+  //};
+
+//module.exports = rect;
+
+exports.perimeter = (x, y) =>{ (2*(x+y)) };
+
+exports.area = (x, y) =>{ (x*y) };
